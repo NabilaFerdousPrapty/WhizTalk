9daae6d94c3b559bd4773ae4c6c194c86d07f713
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-10-12 15:37:24 +0600

    basic frontend backend added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
+const chats = require('./data/data');
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Lets chat with WhizChat');
 }
 );
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+}
+);
+app.get('/api/chat/:id', (req, res) => {
+    const { id } = req.params;
+    const singleChat = chats.find((chat) => chat._id === id);
+    res.send(singleChat);
+
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }
