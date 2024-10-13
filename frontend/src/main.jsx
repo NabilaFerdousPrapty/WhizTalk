7ebb5fa1d6265d5197fe7d9e2e3cae63b3a7f2ca
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-10-14 00:59:19 +0600

    frontend installed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
+import router from "./router/router.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>
 );
